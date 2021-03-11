efcea000a88f9f0099dc44b60ce8f44768bea6eb
Vardan Pogosian <vardan.pogosyan@gmail.com>
2021-03-11 17:14:01 +0300

    advanced build-font.sh

--- build-fonts.sh
+++ build-fonts.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 
-set -exo pipefail
+set -eo pipefail
 
-for p in $(cat plans-to-build.txt | tr $'\n' ' '); do
-	echo "start building $p"
-	npm run build -- ttf::$p
+export PATH="$(pwd)/node_modules/.bin:$PATH"
+
+type="${1%::*}"
+plan="${1#*::}"
+
+[ "$type" == "$plan" ] && type='ttf'
+
+[ ! -z $plan ] && {
+	echo "start building $type::$plan"
+	npm run build -- $type::$plan
+	exit 0
+}
+
+
+plans=($(cat plans-to-build.txt | tr $'\n' ' '))
+for p in ${plans[*]}; do
+	echo "start building $type::$p"
+	npm run build -- $type::$p
 done
