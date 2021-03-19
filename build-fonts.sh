28ffabfb0b6a1ba98daa902987668b57659615fb
Vardan Pogosian <vardan.pogosyan@gmail.com>
2021-03-20 01:29:45 +0300

    imoprved build-fonts.sh

--- build-fonts.sh
+++ build-fonts.sh
@@ -2,6 +2,14 @@
 
 set -eo pipefail
 
+
+function buildPlans() {
+	for p in $1; do
+		echo "start building $type::$p"
+		# npm run build -- $type::$p
+	done
+}
+
 export PATH="$(pwd)/node_modules/.bin:$PATH"
 
 type="${1%::*}"
@@ -10,14 +18,14 @@ plan="${1#*::}"
 [ "$type" == "$plan" ] && type='ttf'
 
 [ ! -z $plan ] && {
-	echo "start building $type::$plan"
-	npm run build -- $type::$plan
-	exit 0
+	plans=$(grep -E "$plan" plans-to-build.txt || true)
+	if [ -z "$plans" ]; then
+		echo "invalid plan $type::$plan"
+		exit 1
+	else
+		buildPlans "$plans"
+		exit 0
+	fi
 }
 
-
-plans=($(cat plans-to-build.txt | tr $'\n' ' '))
-for p in ${plans[*]}; do
-	echo "start building $type::$p"
-	npm run build -- $type::$p
-done
+buildPlans "$(cat plans-to-build.txt | xargs)"
